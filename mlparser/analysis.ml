451e58ef3225f361cd83c3cf298451a958179c9b
Igor Konnov <konnov@forsyte.at>
2012-05-23 15:02:01 +0000

    more changes

--- mlparser/analysis.ml
+++ mlparser/analysis.ml
@@ -1,6 +1,8 @@
 (* Analysis based on abstract interpretation *)
 
 open Cfg;;
+open Spin;;
+open Spin_ir;;
 
 (* general analysis *)
 let visit_basic_block transfer_fun bb in_vals =
@@ -27,7 +29,53 @@ let visit_cfg visit_bb_fun join_fun cfg entry_vals =
 ;;
 
 (* special kind of analysis *)
-type int_role = IntervalInt of int * int | UnboundedInt;;
+type int_role = IntervalInt of int * int | UnboundedInt | Undefined;;
 
-let join_int_roles lhs rhs = lhs
+let lub_int_role x y =
+    match x, y with
+    | Undefined, d -> d
+    | d, Undefined -> d
+    | UnboundedInt, _ -> UnboundedInt
+    | _, UnboundedInt -> UnboundedInt
+    | (IntervalInt (a, b)), (IntervalInt (c, d)) ->
+        IntervalInt ((min a c), (max b  d))
+in
+
+let join_int_roles lhs rhs =
+    let res = Hashtbl.create (Hashtbl.length lhs) in
+    Hashtbl.iter
+        (fun var value ->
+            if Hashtbl.mem rhs var
+            then Hashtbl.add res var (lub_int_role value (Hashtbl.find rhs var))
+            else Hashtbl.add res var value)
+        lhs;
+    Hashtbl.iter
+        (fun var value ->
+            if not (Hashtbl.mem rhs var) then Hashtbl.add res var value)
+        rhs;
+    res
 ;;
+
+let transfer_roles stmt input =
+    let output = Hashtbl.create (Hashtbl.length input)
+    in
+    let rec eval expr = function
+        | Const v -> IntervalInt (v, v)
+        | Var v -> if Hashtbl.mem input v then Hashtbl.find input v else Undefined
+        | UnEx child -> eval child
+        | BinEx (ASGN, Var var, rhs) ->
+            let rhs_val = eval rhs in
+            Hashtbl.add output var rhs_val;
+            rhs_val
+        | BinEx (PLUS, lhs, rhs) -> UnboundedInt (* we are interested in == and != *)
+        | BinEx (MINUS, lhs, rhs) -> UnboundedInt
+        | BinEx (_, _, _) -> Undefined
+        | _ -> Undefined       
+    in
+    match stmt with
+    | Decl (var, init_expr) -> Hashtbl.add var (eval init_expr)
+    | Expr expr -> let _ = eval expr
+    | _ -> ();
+    output
+;;
+
