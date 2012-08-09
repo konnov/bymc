2fba7021533fecdd9e4c280fca0f886c83735ae7
Igor Konnov <konnov@forsyte.at>
2012-08-09 15:26:28 +0000

    removed the useless files lying around

--- ica/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["encode", "lts", "ltsproc", "pretty", "types", "out", "promela",
-           "trace",
-           "promela_lts", "promela_tac", "promela_cfg",
-           "tinypromela_pty", "PrettyLanguageCodeBuilder", "TinyPromelaLexer",
-           "TinyPromelaParser", "TinyPromelaTreeWalker"]
