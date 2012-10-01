fba060096f789670d517a7400f8e76a02a2f8dd4
Igor Konnov <konnov@forsyte.at>
2012-10-01 12:47:00 +0000

    added a dummy for a substitution module

--- bymc/run.ml
+++ bymc/run.ml
@@ -2,39 +2,43 @@ open Printf;;
 
 open Parse;;
 open Abstract;;
+open Substitution;;
 open Writer;;
 open Debug;;
 
+type action_option = OptAbstract | OptRefine | OptCheckInv | OptSubstitute | OptNone
+
 type options =
     {
-        abstract: bool; refine: bool; check_inv: bool;
-        trail_name: string; filename: string; 
-        inv_name: string;
+        action: action_option; trail_name: string; filename: string;
+        inv_name: string; param_assignments: string;
         verbose: bool
     }
 
 let parse_options =
     let opts = ref {
-        abstract = false; refine = false; check_inv = false;
-        trail_name = ""; filename = ""; inv_name = "";
+        action = OptNone; trail_name = ""; filename = ""; inv_name = "";
+        param_assignments = "";
         verbose = false
     } in
     (Arg.parse
         [
-            ("-a", Arg.Unit (fun () -> opts := {!opts with abstract = true}),
+            ("-a", Arg.Unit (fun () -> opts := {!opts with action = OptAbstract}),
              "Produce abstraction of a Promela program.");
             ("-t", Arg.String
-             (fun s -> opts := {!opts with refine = true; trail_name = s}),
+             (fun s -> opts := {!opts with action = OptRefine; trail_name = s}),
              "Check feasibility of a counterexample produced by spin -t (not a *.trail!).");
             ("-i", Arg.String
-             (fun s -> opts := {!opts with check_inv = true; inv_name = s}),
+             (fun s -> opts := {!opts with action = OptCheckInv; inv_name = s}),
              "Check if an atomic proposition is an invariant!.");
+            ("-s", Arg.Unit (fun () ->
+                opts := {!opts with action = OptSubstitute; param_assignments = ""}),
+             "Substitute parameters into the code and produce standard Promela.");
             ("-v", Arg.Unit (fun () -> opts := {!opts with verbose = true}),
              "Produce lots of verbose output (you are warned).");
         ]
-        (fun s ->
-            if !opts.filename = "" then opts := {!opts with filename = s})
-        "Use: run [-a] [-i invariant] [-c spin_sim_out] promela_file");
+        (fun s -> if !opts.filename = "" then opts := {!opts with filename = s})
+        "Use: run [-a] [-i invariant] [-c spin_sim_out] [-e x=num,y=num] promela_file");
 
     !opts
 ;;
@@ -60,13 +64,14 @@ let _ =
         write_to_file "original.prm" units;
         log INFO "  [DONE]";
         log DEBUG (sprintf "#units: %d" (List.length units));
-        if opts.abstract
-        then let _ = do_abstraction true units in ()
-        else if opts.refine
-        then let _ = do_refinement opts.trail_name units in ()
-        else if opts.check_inv
-        then let _ = check_invariant opts.inv_name units in ()
-        else printf "No options given. Bye.\n"
+        match opts.action with
+          OptAbstract -> let _ = do_abstraction true units in ()
+        | OptRefine -> let _ = do_refinement opts.trail_name units in ()
+        | OptCheckInv -> let _ = check_invariant opts.inv_name units in ()
+        | OptSubstitute ->
+            let new_units = do_substitution opts.param_assignments units in
+            write_to_file "concrete.prm" new_units;
+        | _ -> printf "No options given. Bye.\n"
     with End_of_file ->
         log ERROR "Premature end of file";
         exit 1
---
+++ bymc/substitution.ml
@@ -0,0 +1,7 @@
+(*
+ * Substitute parameter values and produce standard Promela code.
+ *)
+
+let do_substitution param_assignments units =
+    units
+;;
