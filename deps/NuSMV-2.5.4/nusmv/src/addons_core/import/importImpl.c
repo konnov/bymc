e17e25d40bdddbc121b59832544a3410bdff5051
Igor Konnov <konnov@forsyte.at>
2013-02-25 23:47:20 +0000

    loading bdds correctly with proper var names

--- deps/NuSMV-2.5.4/nusmv/src/addons_core/import/importImpl.c
+++ deps/NuSMV-2.5.4/nusmv/src/addons_core/import/importImpl.c
@@ -5,44 +5,49 @@
 #include "enc/enc.h"
 #include "enc/bdd/BddEnc.h"
 #include "node/node.h"
+#include "parser/symbols.h"
 
 #include "dddmp.h"
 
 int loadBdd(char* filename) {
-    DdManager * dd_manager;
+    DdManager * dd;
     NodeList_ptr out_vars;
-    ListIter_ptr iter;
+    BddEnc_ptr enc;
     char* root_match_names[] = {"TRANS"};
     char** var_names = NULL;
     DdNode **pproots = NULL;
-    int var_cnt, i = 0, ret = 0;
+    int max_level, i = 0, ret = 0;
 
-    dd_manager = BddEnc_get_dd_manager(Enc_get_bdd_encoding());
-    out_vars = BddEnc_get_var_ordering(Enc_get_bdd_encoding(), DUMP_BITS);
-    var_cnt = NodeList_get_length(out_vars);
-    if ((var_names = malloc(sizeof(char*) * var_cnt)) == NULL) {
+    /* parts of this code go from enc/bdd/BddEnc.c:BddEnc_get_var_ordering */
+    enc = Enc_get_bdd_encoding();
+    dd = BddEnc_get_dd_manager(enc);
+    max_level = dd_get_size(dd);
+    if ((var_names = malloc(sizeof(char*) * max_level)) == NULL) {
         fprintf(nusmv_stderr, "Cannot allocate memory for var_names\n");
         return 1;
     }
 
-    i = 0;
-    iter = NodeList_get_first_iter(out_vars);
-    while (!ListIter_is_end(iter)) {
-        node_ptr name = NodeList_get_elem_at(out_vars, iter);
-        var_names[i++] = util_strsav(sprint_node(name));
-        iter = ListIter_get_next(iter);
+    var_names[0] = NULL; /* 0 is reserved for something */
+    for (i = 1; i < max_level; ++i) {
+        int index = dd_get_index_at_level(dd, i);
+        node_ptr name = BddEnc_get_var_name_from_index(enc, index);
+        /* avoid adding NEXT variables */
+        if (name != Nil && (node_get_type(name) != NEXT))
+            var_names[i] = sprint_node(name);
+        else
+            var_names[i] = NULL;
     }
-    NodeList_destroy(out_vars);
+
     fprintf(nusmv_stderr, "Loading BDDs from %s...\n", filename);
 
-    Dddmp_cuddBddArrayLoad(dd_manager, DDDMP_ROOT_MATCHNAMES,
+    Dddmp_cuddBddArrayLoad(dd, DDDMP_ROOT_MATCHNAMES,
             root_match_names, DDDMP_VAR_MATCHNAMES, var_names, NULL, NULL,
             DDDMP_MODE_DEFAULT, filename, NULL, &pproots);
     fprintf(nusmv_stderr, "Done\n");
 
 clean:
     if (var_names != NULL) {
-        for (i = 0; i < var_cnt; i++)
+        for (i = 0; i < max_level; i++)
             FREE(var_names[i]);
 
         FREE(var_names);
