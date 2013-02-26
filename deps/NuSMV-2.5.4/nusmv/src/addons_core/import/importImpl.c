4258f66dc57d375a33d08936f07cbb5445922e5a
Igor Konnov <konnov@forsyte.at>
2013-02-26 23:27:35 +0000

    loading TRANS correctly (though have a segfault on exit!)

--- deps/NuSMV-2.5.4/nusmv/src/addons_core/import/importImpl.c
+++ deps/NuSMV-2.5.4/nusmv/src/addons_core/import/importImpl.c
@@ -6,18 +6,26 @@
 #include "enc/bdd/BddEnc.h"
 #include "node/node.h"
 #include "parser/symbols.h"
+#include "opt/OptsHandler.h"
 #include "prop/PropDb.h"
 #include "prop/propPkg.h"
 #include "fsm/bdd/BddFsm.h"
+#include "trans/trans.h"
+#include "trans/bdd/BddTrans.h"
+#include "trans/bdd/Cluster.h"
+#include "trans/bdd/ClusterList.h"
 
 #include "dddmp.h"
 
 int loadBdd(char* filename) {
     DdManager * dd;
+    Cluster_ptr cluster;
+    ClusterList_ptr clusters;
     NodeList_ptr out_vars;
     BddEnc_ptr enc;
     BddFsm_ptr bdd_fsm;
     BddTrans_ptr trans;
+    BddVarSet_ptr state_cube, input_cube, next_state_cube;
     char* root_match_names[] = {"TRANS"};
     char** var_names = NULL;
     DdNode **pproots = NULL;
@@ -59,8 +67,31 @@ int loadBdd(char* filename) {
     fprintf(nusmv_stderr, "Overwriting the relations...\n");
     bdd_fsm = PropDb_master_get_bdd_fsm(PropPkg_get_prop_database());
     /* check BddTrans.h, it is actually much more complicated... */
-    trans = BddFsm_get_trans(bdd_fsm);
+    /*trans = BddFsm_get_trans(bdd_fsm);*/
 
+    cluster = Cluster_create(dd);
+    Cluster_set_trans(cluster, dd, pproots[0]);
+    clusters = ClusterList_create(dd);
+    ClusterList_append_cluster(clusters, cluster);
+
+    state_cube = BddEnc_get_state_vars_cube(enc);
+    input_cube = BddEnc_get_input_vars_cube(enc);                            
+    next_state_cube = BddEnc_get_next_state_vars_cube(enc);
+
+    trans = BddTrans_create(dd, clusters, (bdd_ptr) state_cube,
+            (bdd_ptr) input_cube, (bdd_ptr) next_state_cube,
+            TRANS_TYPE_MONOLITHIC,
+            ClusterOptions_create(OptsHandler_get_instance()));
+
+    bdd_fsm = BddFsm_create(BddFsm_get_bdd_encoding(bdd_fsm),
+            BddFsm_get_init(bdd_fsm),
+            BddFsm_get_state_constraints(bdd_fsm),
+            BddFsm_get_input_constraints(bdd_fsm),
+            trans,
+            BddFsm_get_justice(bdd_fsm),
+            BddFsm_get_compassion(bdd_fsm));
+
+    PropDb_master_set_bdd_fsm(PropPkg_get_prop_database(), bdd_fsm);
 clean:
     if (var_names != NULL) {
         for (i = 0; i < max_level; i++)
