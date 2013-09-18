e20215afb8039e9068bb937b16f4eabb1d4ee6e0
Igor Konnov <konnov@forsyte.at>
2013-09-18 19:52:55 +0200

    changes in the build system

--- bymc/make.sh
+++ bymc/make.sh
@@ -48,7 +48,9 @@ case $1 in
   *)
     ocamlbuild -use-ocamlfind $CFLAGS $target | ./script/ocaml-friendly
     find _build -regex '.*\.cm\(i\|o\)'\
-        | sed 's#_build\/src\/##; s/.cm\(i\|o\)//' >./bymc.mltop
+        | sed 's#_build\/src\/##; s/.cm\(i\|o\)//'\
+        >./bymc.mltop
+
     ocamlbuild -use-ocamlfind $CFLAGS ./bymc.top
     ;;
 esac
