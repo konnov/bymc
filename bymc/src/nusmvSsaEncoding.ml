c261cc848c4af2417844191a49f71e671d9cd8ac
Igor Konnov <konnov@forsyte.at>
2013-11-13 16:05:43 +0100

    generating the process description and the defs

--- bymc/src/nusmvSsaEncoding.ml
+++ bymc/src/nusmvSsaEncoding.ml
@@ -13,12 +13,42 @@ open SpinIr
 open SpinIrImp
 open Ssa
 
+(* ============================= utility declarations and functions *)
+
 type proc_var_t =
-    | SharedIn of var
-    | SharedOut of var
-    | LocalIn of var
-    | LocalOut of var
-    | Temp of var
+    | SharedIn of var * data_type
+    | SharedOut of var * data_type
+    | LocalIn of var * data_type
+    | LocalOut of var * data_type
+    | Temp of var * data_type
+
+let proc_var_lt a b =
+    match a, b with
+    | (SharedIn _, SharedOut _)
+    | (SharedIn _, LocalIn _)
+    | (SharedIn _, LocalOut _)
+    | (SharedIn _, Temp _)
+    | (SharedOut _, LocalIn _)
+    | (SharedOut _, LocalOut _)
+    | (SharedOut _, Temp _)
+    | (LocalIn _, LocalOut _)
+    | (LocalIn _, Temp _)
+    | (LocalOut _, Temp _) -> true
+    | (SharedIn (v, _), SharedIn (w, _))
+    | (SharedOut (v, _), SharedOut (w, _))
+    | (LocalIn (v, _), LocalIn (w, _))
+    | (LocalOut (v, _), LocalOut (w, _))
+    | (Temp (v, _), Temp (w, _)) ->
+            (String.compare v#qual_name w#qual_name) < 0
+    | _ -> false
+
+let proc_var_cmp a b =
+    if proc_var_lt a b
+    then -1
+    else if proc_var_lt b a
+    then 1
+    else 0
+
 
 let is_var_temp = function
     | Temp _ -> true
@@ -34,31 +64,46 @@ let is_var_shared_in = function
     | _ -> false
 
 let ptov = function
-    | SharedIn v
-    | SharedOut v
-    | LocalIn v
-    | LocalOut v
-    | Temp v -> v
+    | SharedIn (v, _)
+    | SharedOut (v, _)
+    | LocalIn (v, _)
+    | LocalOut (v, _)
+    | Temp (v, _) -> v
+
+let ptovt = function
+    | SharedIn (v, t)
+    | SharedOut (v, t)
+    | LocalIn (v, t)
+    | LocalOut (v, t)
+    | Temp (v, t) -> v, t
 
 
-let partition_var v =
+let strip_in s = String.sub s 0 ((String.length s) - 3 (* _IN *))
+let strip_out s = String.sub s 0 ((String.length s) - 4 (* _OUT *))
+
+
+let partition_var tt v =
     let is_in = (Str.last_chars v#get_name 3) = "_IN" in
     let is_out = (Str.last_chars v#get_name 4) = "_OUT" in
+    let t = tt#get_type v in
     match (is_in, is_out) with
-    | (true, _) -> if v#proc_name = "" then SharedIn v else LocalIn v
-    | (_, true) -> if v#proc_name = "" then SharedOut v else SharedIn v
-    | _ -> Temp v
+    | (true, _) ->
+            if v#proc_name = "" then SharedIn (v, t) else LocalIn (v, t)
+    | (_, true) ->
+            if v#proc_name = "" then SharedOut (v, t) else SharedIn (v, t)
+    | _ -> Temp (v, t)
 
 
-let replace_with_next syms v =
-    match partition_var v with
-    | SharedOut _ ->
-        let nm = v#get_name in
-        let inm = (String.sub nm 0 ((String.length nm) - 4)) ^ "_IN" in
+let replace_with_next syms tt v =
+    match partition_var tt v with
+    | SharedOut (_, _) ->
+        let inm = (strip_out v#get_name) ^ "_IN" in
         UnEx (NEXT, Var ((syms#lookup inm)#as_var))
 
     | _ -> Var v 
 
+
+(* ====================== important functions *)
 
 let module_of_proc rt prog proc =
     let vars_of_syms syms =
@@ -86,19 +131,42 @@ let module_of_proc rt prog proc =
     in
     let ntt, syms, exprs = to_ssa in
     let new_vars = vars_of_syms syms#get_symbs in
-    let exprs = List.map (map_vars (replace_with_next syms)) exprs in
-    let pvars = List.map partition_var new_vars in
-    let temps = List.map ptov (List.filter is_var_temp pvars) in
-    let args = List.map ptov (List.filter
-        (fun pv -> is_var_local pv || is_var_shared_in pv) pvars) in
-    let temp_decls = List.map (fun v -> (v, ntt#get_type v)) temps in
-    Module (proc#get_name, args, [SVar temp_decls; STrans exprs])
+    let exprs = List.map (map_vars (replace_with_next syms ntt)) exprs in
+    let pvars = List.sort proc_var_cmp (List.map (partition_var ntt) new_vars) in
+    let temps = List.filter is_var_temp pvars in
+    let args =
+        List.filter (fun pv -> is_var_local pv || is_var_shared_in pv) pvars in
+    let mod_type =
+        Module (proc#get_name,
+            List.map ptov args, [SVar (List.map ptovt temps); STrans exprs])
+    in
+    (mod_type, args)
 
 
 let transform rt out_name intabs_prog =
-    let procs = Program.get_procs intabs_prog in
-    let tops = List.map (module_of_proc rt intabs_prog) procs in
     let out = open_out (out_name ^ ".smv") in
+    let transform_proc (globals, main_sects) p =
+        let mod_type, args = module_of_proc rt intabs_prog p in
+        let to_param = function
+            | SharedIn (v, t) -> (v#copy (strip_in v#get_name), t)
+            | SharedOut (v, t) -> (v#copy (strip_out v#get_name), t)
+            | LocalIn (v, t)
+            | LocalOut (v, t) -> (v, t)
+            | _ -> raise (Failure ("Unexpected param type"))
+        in
+        let params = List.map to_param args in
+        let inst = SModInst("p_" ^ p#get_name, p#get_name, List.map fst params)
+        in
+        let locals = List.filter (fun (v, _) -> v#proc_name <> "") params in
+        (mod_type :: globals, (SVar locals) :: inst :: main_sects)
+    in
+    let procs = Program.get_procs intabs_prog in
+    let tt = Program.get_type_tab intabs_prog in
+    let globals, main_sects = List.fold_left transform_proc ([], []) procs in
+    let shared =
+        List.map (fun v -> (v, tt#get_type v)) (Program.get_shared intabs_prog)
+    in
+    let tops = Module ("main", [], (SVar shared) :: main_sects) :: globals in
     List.iter (fun t -> fprintf out "%s\n" (top_s t)) tops;
     close_out out
 
