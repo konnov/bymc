ae78d8b834988cc34643f1e20eff1c2072dd7045
Igor Konnov <konnov@forsyte.at>
2013-11-25 14:47:44 +0100

    verification with bdds

---
+++ bymc/mod-verify-nusmv-bdd.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Functions specific to nusmv using bdds
+#
+# Igor Konnov, 2013
+
+NUSMV=${NUSMV:-nusmv}
+BYMC_FLAGS="--target nusmv"
+
+SRC_REACH="main-ssa-reach.smv"
+SRC="main-ssa.smv"
+HIDDEN="main-ssa-hidden.txt"
+
+function mc_compile_first {
+    rm -f "$HIDDEN"
+    echo "Generating initial abstraction..."
+    CAMLRUNPARAM="b" ${TOOL} ${BYMC_FLAGS} -a ${PROG} \
+        || report_and_quit "Failure: ${TOOL} -a ${PROG}"
+    echo "[DONE]"
+    echo "Checking reachability of the local states..."
+    ${BYMC_HOME}/nusmv-find-reach "${NUSMV}" "${SRC_REACH}" "${HIDDEN}"
+    echo "[DONE]"
+    echo ""
+}
+
+function mc_verify_spec {
+    SCRIPT="script.nusmv"
+    echo "set on_failure_script_quits" >$SCRIPT
+    echo "go" >>$SCRIPT
+    if grep -q "INVARSPEC NAME ${PROP}" "${SRC}"; then
+        echo "check_invar -P ${PROP}" >>${SCRIPT}
+    else
+        echo "check_ltlspec -P ${PROP}" >>${SCRIPT}
+    fi
+    echo "show_traces -v -o ${CEX}" >>${SCRIPT}
+    echo "quit" >>${SCRIPT}
+
+    rm -f ${CEX}
+    ${NUSMV} -df -v $NUSMV_VERBOSE -source "${SCRIPT}" "${SRC}" | tee "${MC_OUT}" \
+        || report_and_quit "nusmv failed"
+    # the exit code of grep is the return code
+    if grep -q "is true" ${MC_OUT}; then
+        echo ""
+        echo "Specification holds true."
+        echo ""
+        true
+    else
+        false
+    fi
+}
+
+function mc_refine {
+    CAMLRUNPARAM="b" ${TOOL} -t ${CEX} 2>&1 | tee refinement.out
+    echo ""
+}
+
+function mc_collect_stat {
+    # TODO: collect the statistics
+    mc_stat="|technique=nusmv-bdd"
+}
+
