77a4c0d2b3ed71e89261751d98ad04a915f96bfb
Igor Konnov <konnov@forsyte.at>
2013-11-29 23:47:08 +0100

    testing ssa somewhat similar to bcast-byz

--- bymc/test/ssaTest.ml
+++ bymc/test/ssaTest.ml
@@ -39,7 +39,8 @@ let teardown _ =
     ()
 
 
-let test_optimize_ssa1 _ =
+(* the scary bug I came up with on Nov 28, 2013 *)
+let test_optimize_ssa_in_out _ =
     let x = new_var "x" in
     let entry = mk_entry x in
     let b1 = mk_bb x 1 4 [1] in
@@ -71,29 +72,65 @@ let test_optimize_ssa1 _ =
     let cfg = new control_flow_graph entry
             [b5; b7; exit; b1; b8; b6; b3; entry; b2; b4]
     in
-    ignore (optimize_ssa cfg);
+    ignore (optimize_ssa cfg [x]);
+
     let expect_no_phi b =
         match b#get_seq with
-        | [Label (_, _); Skip _] ->
+        | [Label (_, _)] ->
             () (* ok *)
-        | _ -> raise (Failure ("expected no phi"))
+        | _ -> assert_failure
+          (sprintf "unexpected sequence in %d: %s" b#label
+            (str_join "; " (List.map stmt_s b#get_seq)))
     in
-    List.iter expect_no_phi [b3; b5; b6; b7; b8];
-    let expect_asgn b i =
+    List.iter expect_no_phi [b5; b6; b7; b8];
+    let expect_phi_asgn b expect_lhs expect_rhs =
         match b#get_seq with
-        | [Label (_, _); Skip _;
+        | [Label (_, _);
+                Expr (_, BinEx (ASGN, Var lhs, Var rhs))] ->
+            assert_equal
+                ~msg:(sprintf "expected x_T%d = x_T%d, found %d, %d"
+                    expect_lhs expect_rhs lhs#mark rhs#mark)
+                expect_lhs lhs#mark;
+            assert_equal
+                ~msg:(sprintf "expected x_T%d = x_T%d, found %d, %d"
+                    expect_lhs expect_rhs lhs#mark rhs#mark)
+                expect_rhs rhs#mark
+        | _ as seq ->
+            assert_failure
+                (sprintf "unexpected sequence at %d: %s"
+                    b#label (str_join "; " (List.map stmt_s seq)))
+    in
+    expect_phi_asgn b3 Ssa.mark_out 4;
+    let expect_skip_asgn b i =
+        match b#get_seq with
+        | [Label (_, _);
                 Expr (_, BinEx (ASGN, Var lhs, Const i))] ->
             assert_equal
                 ~msg:(sprintf "expected x_OUT = i, found %d" lhs#mark)
                 lhs#mark Ssa.mark_out
-        | _ -> raise (Failure ("expected label; skip; assign"))
+        | _ as seq ->
+            assert_failure
+                (sprintf "expected label in block %d; assign, found: %s"
+                    b#label (str_join "; " (List.map stmt_s seq)))
     in
-    expect_asgn b4 3;
+    expect_skip_asgn b4 3;
+    let expect_asgn b i =
+        match b#get_seq with
+        | [Label (_, _); Expr (_, BinEx (ASGN, Var lhs, Const i))] ->
+            assert_bool
+                (sprintf "expected x_j = i and j <> OUT, found %d" lhs#mark)
+                (lhs#mark <> Ssa.mark_out)
+        | _ as seq ->
+            assert_failure
+                (sprintf "expected label in block %d; assign, found: %s"
+                    b#label (str_join "; " (List.map stmt_s seq)))
+    in
     expect_asgn b1 1;
     expect_asgn b2 2
 
 
-let test_reduce_indices1 _ =
+(* the coloring works as expected on diamonds *)
+let test_reduce_indices_diamond _ =
     let x = new_var "x" in
     let entry = mk_entry x in
     let b1 = mk_bb x 1 2 [1] in
@@ -117,13 +154,13 @@ let test_reduce_indices1 _ =
         match b1#get_seq with
         | [Label (_, _); Expr (_, Phi (lhs, rhs))] ->
             assert_equal ~msg:"b1: lhs#mark = 1" lhs#mark 1
-        | _ -> raise (Failure ("expected phi"))
+        | _ -> assert_failure ("expected phi")
     end;
     begin
         match b2#get_seq with
         | [Label (_, _); Expr (_, Phi (lhs, rhs))] ->
             assert_equal ~msg:"b2: lhs#mark = 1" lhs#mark 1
-        | _ -> raise (Failure ("expected phi"))
+        | _ -> assert_failure ("expected phi")
     end;
     begin
         match exit#get_seq with
@@ -136,15 +173,91 @@ let test_reduce_indices1 _ =
                         (List.map (fun v -> string_of_int v#mark) rhs));
                 assert_equal ~msg:"exit: rhs marks = [1; 1]"
                     (List.map (fun v -> v#mark) rhs) [1; 1];
-        | _ -> raise (Failure ("unexpected sequence in exit: "
-            ^ (str_join "; " (List.map stmt_s exit#get_seq))))
+        | _ -> assert_failure ("unexpected sequence in exit: "
+            ^ (str_join "; " (List.map stmt_s exit#get_seq)))
     end
+
+
+let test_mk_ssa _ =
+    let x = new_var "x" in
+    let id _ = fresh_id () in
+    (* this code is very similar to next_nrcvd in bcast-byz.pml *)
+    let code =
+        MIf (fresh_id (), [
+            MOptGuarded[ MExpr (id(), BinEx (EQ, Var x, Const 0)) ];
+            MOptGuarded[ MExpr (id(), BinEx (NE, Var x, Const 1)) ];
+        ])
+        ::
+        MIf (fresh_id (), [
+            MOptGuarded[ MExpr (id(), BinEx (ASGN, Var x, Const 0)) ];
+            MOptGuarded[ MExpr (id(), BinEx (ASGN, Var x, Const 1)) ];
+            MOptGuarded[ MExpr (id(), BinEx (ASGN, Var x, Const 2)) ];
+            MOptGuarded[ MExpr (id(), BinEx (ASGN, Var x, Const 3)) ];
+            MOptGuarded[ MExpr (id(), BinEx (ASGN, Var x, Const 4)) ];
+            MOptGuarded[ MExpr (id(), BinEx (ASGN, Var x, Const 5)) ];
+            MOptGuarded[ MExpr (id(), BinEx (ASGN, Var x, Const 6)) ];
+            MOptGuarded[ MExpr (id(), BinEx (ASGN, Var x, Const 7)) ];
+            MOptGuarded[ MExpr (id(), BinEx (ASGN, Var x, Const 8)) ];
+        ])
+        ::
+        MIf (fresh_id (), [
+            MOptGuarded[
+                MExpr (id(), BinEx (EQ, Var x, Const 0));
+                MExpr (id(), BinEx (ASGN, Var x, Const 5))
+            ];
+            MOptGuarded[ MExpr (id(), BinEx (NE, Var x, Const 1)) ];
+        ])
+        ::
+        MIf (fresh_id (), [
+            MOptGuarded[ MExpr (id(), BinEx (EQ, Var x, Const 3)) ];
+            MOptGuarded[ MExpr (id(), BinEx (EQ, Var x, Const 2)) ];
+            MOptGuarded[ MExpr (id(), BinEx (EQ, Var x, Const 1)) ];
+        ])
+        ::
+        MIf (fresh_id (), [
+            MOptGuarded[ MExpr (id(), BinEx (EQ, Var x, Const 3)) ];
+            MOptGuarded[
+                MExpr (id(), BinEx (EQ, Var x, Const 4));
+                MIf (fresh_id (), [
+                    MOptGuarded [ MSkip (id ()) ];
+                    MOptGuarded [ MSkip (id ()) ];
+                    MOptGuarded [ MSkip (id ()) ]
+                ])
+            ];
+        ])
+        :: []
+    in
+    let cfg = Cfg.remove_ineffective_blocks (mk_cfg (mir_to_lir code))
+    in
+    let nst = new symb_tab "" in
+    let ntt = new data_type_tab in
+    ntt#set_type x (mk_int_range 0 9);
+    let solver = new yices_smt in
+    solver#start;
+    let cfg_ssa = mk_ssa solver false [x] [] nst ntt cfg in
+    ignore (solver#stop);
+    Cfg.write_dot "ssa-test.dot" cfg_ssa;
+    let collect us b =
+        let used = stmt_list_used_vars b#get_seq in
+        List.fold_left (fun s v -> StringSet.add v#get_name s) us used
+    in
+    let used_set =
+        List.fold_left collect StringSet.empty cfg_ssa#block_list
+    in
+    assert_equal ~msg:"|used_set| = 4" 4 (StringSet.cardinal used_set);
+    assert_bool "x_IN in used_set" (StringSet.mem "x_IN" used_set);
+    assert_bool "x_OUT in used_set" (StringSet.mem "x_OUT" used_set);
+    assert_bool "x_T2 in used_set" (StringSet.mem "x_T2" used_set);
+    assert_bool "x_T6 in used_set" (StringSet.mem "x_T6" used_set)
+
 
 let suite = "ssa-suite" >:::
     [
-        "test_optimize_ssa1"
-            >:: (bracket setup test_optimize_ssa1 teardown);
-        "test_reduce_indices1"
-            >:: (bracket setup test_reduce_indices1 teardown)
+        "test_optimize_ssa_in_out"
+            >:: (bracket setup test_optimize_ssa_in_out teardown);
+        "test_reduce_indices_diamond"
+            >:: (bracket setup test_reduce_indices_diamond teardown);
+        "test_mk_ssa"
+            >:: (bracket setup test_mk_ssa teardown)
     ]
 
