fc14111b69cd535af03106214b27464565ed381b
Igor Konnov <konnov@forsyte.at>
2013-12-09 18:57:41 +0100

    reformatting the bits

--- bymc/src/bits.ml
+++ bymc/src/bits.ml
@@ -1,22 +1,20 @@
 (*
-  Bit-vector structures encoded in SAT.
-  Should make SAT encoding easier to us.
+  Operations on bit vectors encoded as SAT.
 
-  Open this module with care as it introduces lots of shortcut symbols.
-
-  Igor Konnov, 2011
+  Igor Konnov, 2011-2013
  *)
 
-open List;;
-open Printf;;
+open List
+open Printf
 
-open Sat;;
-open Accums;;
+open Sat
+open Accums
 
-exception VarNotFound of string;;
+exception VarNotFound of string
 
 (* a convenient language over bit vectors *)
-type bv_form = B1 | B0 | V of string | NV of string
+type bv_form =
+    | B1 | B0 | V of string | NV of string
     | EQ of string * string | NE of string * string
     | LT of string * string | LE of string * string
     | GT of string * string | GE of string * string
@@ -24,13 +22,11 @@ type bv_form = B1 | B0 | V of string | NV of string
     | VneI of string * int
     | AND of bv_form list | OR of bv_form list
     | ANNOTATION of string * bv_form (* a comment wrapping a subformula *)
-;;
 
 type bitvar_type = { len: int; hidden: bool }
 
-let rec bv_form_s bv =
-    match bv with
-        B1 -> "1"
+let rec bv_form_s = function
+      | B1 -> "1"
       | B0 -> "0"
       | V x -> x
       | NV x -> "!" ^ x
@@ -42,17 +38,26 @@ let rec bv_form_s bv =
       | GE(x, y) -> x ^ " >= " ^ y
       | VeqI(x, ival) -> sprintf "%s = %d" x ival
       | VneI(x, ival) -> sprintf "%s != %d" x ival
-      | AND lst -> List.fold_left
-        (fun s f ->
-            (if s = "" then "(" else s ^ " & (") ^ (bv_form_s f) ^ ")") "" lst
-      | OR lst -> List.fold_left
-        (fun s f -> (if s = "" then "(" else s ^ " | (") ^ (bv_form_s f) ^ ")") "" lst
-      | ANNOTATION(_, subform) -> bv_form_s subform
-;;
 
-let rec neg_bf_form bv =
-    match bv with
-        B1 -> B1
+      | AND lst ->
+            let concat s f =
+                let pref = if s <> "" then " & " else "" in
+                (pref ^ "(" ^ (bv_form_s f) ^ ")")
+            in
+            List.fold_left concat "" lst
+
+      | OR lst ->
+            let concat s f =
+                let pref = if s <> "" then " | " else "" in
+                (pref ^ "(" ^ (bv_form_s f) ^ ")")
+            in
+            List.fold_left concat "" lst
+
+      | ANNOTATION(_, subform) -> bv_form_s subform
+
+
+let rec neg_bf_form = function
+      | B1 -> B1
       | B0 -> B1
       | V x -> NV x
       | NV x -> V x
@@ -64,15 +69,14 @@ let rec neg_bf_form bv =
       | GE(x, y) -> LT(x, y)
       | VeqI(x, ival) -> VneI(x, ival)
       | VneI(x, ival) -> VeqI(x, ival)
-      | AND lst -> OR(List.rev_map neg_bf_form lst)
-      | OR lst -> AND(List.rev_map neg_bf_form lst)
-      | ANNOTATION(text, subform) -> ANNOTATION(text, neg_bf_form subform)
-;;
+      | AND lst -> OR (List.rev_map neg_bf_form lst)
+      | OR lst -> AND (List.rev_map neg_bf_form lst)
+      | ANNOTATION(text, subform) ->
+          ANNOTATION(text, neg_bf_form subform)
 
 
-let rec format_bv_form ff bv =
-    match bv with
-        B1 -> Format.fprintf ff "1"
+let rec format_bv_form ff = function
+      | B1 -> Format.fprintf ff "1"
       | B0 -> Format.fprintf ff "0"
       | V x -> Format.fprintf ff "%s" x
       | NV x -> Format.fprintf ff "~%s" x
@@ -86,30 +90,28 @@ let rec format_bv_form ff bv =
       | VneI(x, ival) -> Format.fprintf ff "%s != %d" x ival
       | AND lst ->
         Format.fprintf ff "@[<1>";
-        let first = ref true in
-        (List.iter
-            (fun sub ->
-                if !first
-                then Format.fprintf ff "%s" "("
-                else Format.fprintf ff "@ @,&@ %s" "(";
-                first := false;
-                (format_bv_form ff sub);
-                Format.fprintf ff ")")
-            lst);
-        Format.fprintf ff "@]";
+        let print first form =
+            if first
+            then Format.fprintf ff "%s" "("
+            else Format.fprintf ff "@ @,&@ %s" "(";
+            format_bv_form ff form;
+            Format.fprintf ff ")";
+            false
+        in
+        List.fold_left print true lst;
+        Format.fprintf ff "@]"
       | OR lst ->
         Format.fprintf ff "@[<1>";
-        let first = ref true in
-        (List.iter
-            (fun sub ->
-                if !first
-                then Format.fprintf ff "%s" "("
-                else Format.fprintf ff "@ @,|@ %s" "(";
-                first := false;
-                (format_bv_form ff sub);
-                Format.fprintf ff ")")
-            lst);
-        Format.fprintf ff "@]";
+        let print first form =
+            if first
+            then Format.fprintf ff "%s" "("
+            else Format.fprintf ff "@ @,|@ %s" "(";
+            format_bv_form ff form;
+            Format.fprintf ff ")";
+            false
+        in
+        List.fold_left print true lst;
+        Format.fprintf ff "@]"
       | ANNOTATION(text, subform) ->
         Format.fprintf ff "  /* %s */@\n" text;
         format_bv_form ff subform
@@ -182,7 +184,6 @@ let rec mk_lt x y x_sz y_sz carry_z aux_pool =
                  Or[Not(Var(ivar_s x x_sz)); aux_var];
                  Or[Not(Var(ivar_s y y_sz)); aux_var]]
             (mk_lt x y (x_sz - 1) (y_sz - 1) aux_var aux_pool))
-;;
 
 
 let rec mk_le x y x_sz y_sz carry_z aux_pool =
@@ -201,7 +202,6 @@ let rec mk_le x y x_sz y_sz carry_z aux_pool =
                  Or[Not(Var(ivar_s x x_sz)); aux_var];
                  Or[Not(Var(ivar_s y y_sz)); aux_var]]
             (mk_le x y (x_sz - 1) (y_sz - 1) aux_var aux_pool))
-;;
 
 
 let rec mk_eq_with_const x len ival positive =
@@ -216,7 +216,7 @@ let rec mk_eq_with_const x len ival positive =
         in
     let collected = (to_bin x len 1 ival []) in
     if positive = 1 then And(collected) else Or(collected)
-;;
+
 
 (*
   Transform a bit-vector formula to a boolean formula
@@ -224,13 +224,11 @@ let rec mk_eq_with_const x len ival positive =
 *)
 let rec to_sat bv_types aux_pool bv_frm =
     let get_var_len x =
-        try
-            (StringMap.find x bv_types).len
-        with Not_found ->
-            raise(VarNotFound x)
+        try (StringMap.find x bv_types).len
+        with Not_found -> raise (VarNotFound x)
     in
     match bv_frm with
-        B1 -> True
+      | B1 -> True
       | B0 -> False
       | V x ->
             if (get_var_len x) = 1
@@ -269,7 +267,7 @@ let rec to_sat bv_types aux_pool bv_frm =
               Or(List.rev (List.rev_map (to_sat bv_types aux_pool) lst));
       | ANNOTATION(_, subform) ->
               to_sat bv_types aux_pool subform
-;;
+
 
 let bitsat_to_cnf bit_vars aux_pool bitsat_form =
     let first_id = aux_pool#get_free_id in
@@ -277,7 +275,7 @@ let bitsat_to_cnf bit_vars aux_pool bitsat_form =
     let next_id = aux_pool#get_free_id in
     { sat_form = cnf_form; first_id = first_id;
       num_aux_vars = (next_id - first_id) }
-;;
+
 
 let map_bit_vars_to_dimacs bit_vars first_id =
     let map_one name bit_type (i_map, i_id) =
@@ -297,7 +295,6 @@ let map_bit_vars_to_dimacs bit_vars first_id =
         map_one
         bit_vars
         (StringMap.empty, first_id)
-;;
 
 
 (*
@@ -321,7 +318,6 @@ let mk_state_vector_ids bit_vars bool_vars layer_no =
                 (Printf.sprintf "Bool variable %s not found" n))
     in
     List.rev_map get_var_index names
-;;
 
 
 let bits_to_fit n =
@@ -331,7 +327,6 @@ let bits_to_fit n =
         else f (b + 1) (2 * m)
     in
     f 1 2
-;;
 
 
 let fold_bit_var lit_lst pref =
@@ -345,5 +340,4 @@ let fold_bit_var lit_lst pref =
             else sum)
         0
         lit_lst
-;;
 
