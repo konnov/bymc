0321d78da41906358f045c56bc99c5c968e06ba0
Igor Konnov <konnov@forsyte.at>
2014-04-15 18:01:47 +0200

    fixed the intersection w/o using ocamlgraph + the definitions

--- bymc/src/porBounds.ml
+++ bymc/src/porBounds.ml
@@ -15,24 +15,43 @@ open SymbSkel
 module IGraph = Graph.Pack.Digraph
 module IGraphOper = Graph.Oper.I(IGraph)
 
+(* as ocamlgraph does distinguish two vertices objects that are labelled the
+   same, we have to create a universum of all these vertices (and edges).
+ *)
+
+module U = struct
+    let vertices = Hashtbl.create 1000
+    let edges = Hashtbl.create 10000
+
+    let mkv i =
+        try Hashtbl.find vertices i
+        with Not_found ->
+            let v = IGraph.V.create i in
+            Hashtbl.replace vertices i v; v
+
+    let mke i lab j =
+        try Hashtbl.find edges (i, lab, j)
+        with Not_found ->
+            let e = IGraph.E.create (mkv i) lab (mkv j) in
+            Hashtbl.replace edges (i, lab, j) e; e
+end
+
+
 type lock_t = Lock | Unlock
 
 let compute_flow sk =
     let flowg = IGraph.create () in
-    let incoming = Hashtbl.create sk.Sk.nrules in
+    let outgoing = Hashtbl.create sk.Sk.nrules in
     let addi (i, r) =
-        IGraph.add_vertex flowg (IGraph.V.create i);
-        if Hashtbl.mem incoming r.Sk.src
-        then Hashtbl.replace incoming r.Sk.src (i :: (Hashtbl.find incoming r.Sk.src))
-        else Hashtbl.add incoming r.Sk.src [i]
+        IGraph.add_vertex flowg (U.mkv i);
+        if Hashtbl.mem outgoing r.Sk.src
+        then Hashtbl.replace outgoing r.Sk.src (i :: (Hashtbl.find outgoing r.Sk.src))
+        else Hashtbl.add outgoing r.Sk.src [i]
     in
     List.iter addi (lst_enum sk.Sk.rules);
     let add_flow (i, r) =
-        let each_succ j =
-            IGraph.add_edge flowg
-                (IGraph.find_vertex flowg i) (IGraph.find_vertex flowg j)
-        in
-        try List.iter each_succ (Hashtbl.find incoming r.Sk.dst)
+        let each_succ j = IGraph.add_edge_e flowg (U.mke i 0 j) in
+        try List.iter each_succ (Hashtbl.find outgoing r.Sk.dst)
         with Not_found -> ()
     in
     List.iter add_flow (lst_enum sk.Sk.rules);
@@ -40,7 +59,7 @@ let compute_flow sk =
     flowg
 
 
-let does_r_affect_t solver shared lockt r t =
+let does_r_affect_t solver shared lockt i r j t =
     let mk_layer i =
         let h = Hashtbl.create (List.length shared) in
         let add v =
@@ -81,17 +100,28 @@ let does_r_affect_t solver shared lockt r t =
     in
     (* the variable declarations must be moved out of the function *)
     List.iter (decl l0) shared; List.iter (decl l1) shared;
+    ignore (solver#comment (sprintf "does rule %d %s rule %d?"
+            i (if lockt = Unlock then "unlock" else "lock") j));
     if not (is_c_true r_pre0)
-    then ignore (solver#append_expr r_pre0); (* r is enabled *)
+    then begin
+        ignore (solver#comment "r is enabled");
+        ignore (solver#append_expr r_pre0); (* r is enabled *)
+    end;
     if lockt = Unlock
     then begin
+        ignore (solver#comment "t is disabled");
         ignore (solver#append_expr (UnEx (NEG, t_pre0))); (* t is not *)
+        ignore (solver#comment "r fires");
         ignore (solver#append_expr r_post0); (* r fires *)
+        ignore (solver#comment "t is now enabled");
         ignore (solver#append_expr t_pre1); (* t becomes enabled *)
     end else if lockt = Lock
     then begin
+        ignore (solver#comment "t is enabled");
         ignore (solver#append_expr (t_pre0)); (* t is enabled *)
+        ignore (solver#comment "r fires");
         ignore (solver#append_expr r_post0); (* r fires *)
+        ignore (solver#comment "t is now disabled");
         ignore (solver#append_expr (UnEx (NEG, t_pre1))); (* t becomes disabled *)
     end else begin
         raise (Failure "Unsupported lock type")
@@ -101,18 +131,18 @@ let does_r_affect_t solver shared lockt r t =
     res
 
 
-let compute_unlocking lockt solver sk =
+let compute_unlocking lockt is_to_keep solver sk =
     let graph = IGraph.create () in
     let add_rule i =
-        IGraph.add_vertex graph (IGraph.V.create i)
+        IGraph.add_vertex graph (U.mkv i)
     in
     List.iter add_rule (range 0 sk.Sk.nrules);
     let add_flow (i, r) =
         let each (j, t) =
             if i <> j && not (is_c_true t.Sk.guard)
-                && does_r_affect_t solver sk.Sk.shared lockt r t
-            then IGraph.add_edge graph
-                (IGraph.find_vertex graph i) (IGraph.find_vertex graph j)
+                && does_r_affect_t solver sk.Sk.shared lockt i r j t
+                && is_to_keep i j
+            then IGraph.add_edge_e graph (U.mke i 0 j)
         in
         List.iter each (lst_enum sk.Sk.rules)
     in
@@ -129,29 +159,34 @@ let compute_diam solver dom_size sk =
     let fg = compute_flow sk in
     let nflow = IGraph.nb_edges fg in
     logtm INFO (sprintf "> %d transition flow dependencies" nflow);
-    let not_fgc =
-        IGraphOper.complement (IGraphOper.transitive_closure ~reflexive:false fg) in
+    let fgp = IGraphOper.transitive_closure ~reflexive:false fg in
+    IGraph.dot_output fg (sprintf "flow-%s.dot" sk.Sk.name);
+    IGraph.dot_output fgp (sprintf "flowplus-%s.dot" sk.Sk.name);
+    let is_in_flowplus i j =
+        (* find_vertex is inefficient, but the authors of ocamlgraph don't
+           leave us any other choice... *)
+        IGraph.mem_edge fgp (IGraph.find_vertex fgp i) (IGraph.find_vertex fgp j)
+    in
+    let is_backward i j = not (is_in_flowplus i j) && (is_in_flowplus j i)
+    in
     logtm INFO (sprintf "> constructing unlocking transition dependencies...");
-    let ug = compute_unlocking Unlock solver sk in
+    let ug = compute_unlocking Unlock is_backward solver sk in
     logtm INFO (sprintf "> %d unlocking transition dependencies" (IGraph.nb_edges ug));
-    let diff = IGraphOper.intersect ug not_fgc in
-    let nbackward = IGraph.nb_edges diff in
+    let nbackward = IGraph.nb_edges ug in
     logtm INFO (sprintf "> %d backward unlocking transition dependencies" nbackward);
-    IGraph.dot_output diff
-        (sprintf "unlocking-flowplus-%s.dot" sk.Sk.name);
+    IGraph.dot_output ug (sprintf "unlocking-flowplus-%s.dot" sk.Sk.name);
 
+    let is_forward i j = not (is_in_flowplus j i) && (is_in_flowplus i j)
+    in
     logtm INFO (sprintf "> constructing locking transition dependencies...");
-    let lg = compute_unlocking Lock solver sk in
+    let lg = compute_unlocking Lock is_forward solver sk in
     logtm INFO (sprintf "> %d locking transition dependencies" (IGraph.nb_edges lg));
-    let diff = IGraphOper.intersect (IGraphOper.mirror lg) not_fgc in
-    let nforward = IGraph.nb_edges diff in
+    let nforward = IGraph.nb_edges lg in
     logtm INFO (sprintf "> %d forward locking transition dependencies" nforward);
-    IGraph.dot_output diff
-        (sprintf "locking-flowplus-%s.dot" sk.Sk.name);
+    IGraph.dot_output lg (sprintf "locking-flowplus-%s.dot" sk.Sk.name);
 
     (* XXX: the bound is lower than that, find the identical conditions
-      instead of just backward edges
-     *)
+      instead of just backward edges *)
     let bound = (1 + nbackward + nforward) * sk.Sk.nrules in
     log INFO (sprintf "> the bound for %s is %d = (1 + %d + %d) * %d"
         sk.Sk.name bound nbackward nforward sk.Sk.nrules);
