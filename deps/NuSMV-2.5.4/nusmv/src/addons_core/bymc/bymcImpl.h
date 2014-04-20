d5ab6c365f01ac430a85e3698d2f116ff76d002a
Igor Konnov <konnov@forsyte.at>
2014-04-20 13:08:29 +0200

    removed nusmv from the dependencies

--- deps/NuSMV-2.5.4/nusmv/src/addons_core/bymc/bymcImpl.h
+++
@@ -1,6 +0,0 @@
-#ifndef BYMC_IMPL_H
-#define BYMC_IMPL_H
-
-int loadBdd(char* filename);
-
-#endif /* BYMC_IMPL_H */
--- deps/NuSMV-2.5.4/nusmv/src/addons_core/import/importImpl.h
+++
@@ -1,6 +0,0 @@
-#ifndef IMPORT_IMPL_H
-#define IMPORT_IMPL_H
-
-int loadBdd(char* filename);
-
-#endif /* IMPORT_IMPL_H */
