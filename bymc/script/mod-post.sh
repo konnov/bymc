cb272c5e78ba6cd47ef2412c106ccfeffe140b9a
Igor Konnov <konnov@forsyte.at>
2015-01-25 16:26:41 +0100

    refactored the output

--- bymc/script/mod-post.sh
+++ bymc/script/mod-post.sh
@@ -2,7 +2,7 @@
 #
 # Functions specific to partial order schedule tree
 #
-# Igor Konnov, 2014
+# Igor Konnov, 2014-2015
 
 NUSMV=${NUSMV:-nusmv}
 POST_OUT="post.out"
@@ -38,35 +38,39 @@ function mc_collect_stat {
         res="SUCCESS"
     fi
 
-    time_stat=`grep maxresident $POST_OUT | tail -n 1 | perl -n -e 'if (/(.*)user (.*)system (.*)elapsed.*avgdata\D*(\d+)maxresident.*/) { print "$1 $2 $3 $4\n" }'`
-    b_user=`echo $time_stat | cut -d ' ' -f 1`
-    b_sys=`echo $time_stat | cut -d ' ' -f 2`
-    b_elapsed=`echo $time_stat | cut -d ' ' -f 3`
-    b_maxres=`echo $time_stat | cut -d ' ' -f 4`
-    bound=`grep "the bound for" $POST_OUT | tail -n 1 \
-        | perl -n -e 'if (/.*bound for.*is (\d+) =.*/) { print "$1\n" }'`
-    mbound=`grep "the mild bound for" $POST_OUT | tail -n 1 \
-        | perl -n -e 'if (/.*bound for.*is (\d+).*/) { print "$1\n" }'`
-    cabound=`grep "the counter abstraction bound for" $POST_OUT | tail -n 1 \
-        | perl -n -e 'if (/.*bound for.*is (\d+) =.*/) { print "$1\n" }'`
-    mcabound=`grep "the mild counter abstraction bound for" $POST_OUT | tail -n 1 \
-        | perl -n -e 'if (/.*bound for.*is (\d+).*/) { print "$1\n" }'`
-    unlocking=`grep "[0-9] unlocking milestones" $POST_OUT | tail -n 1 \
-        | perl -n -e 'if (/.*?(\d+) unlocking.*/) { print "$1\n" }'`
-    locking=`egrep "[0-9] locking milestones" $POST_OUT | tail -n 1 \
-        | perl -n -e 'if (/.*?(\d+) locking.*/) { print "$1\n" }'`
-    nlocs=`grep "locations" $POST_OUT | tail -n 1 \
-        | perl -n -e 'if (/.*found (\d+) locations.*/) { print "$1\n" }'`
-    nrules=`grep "rules" $POST_OUT | tail -n 1 \
-        | perl -n -e 'if (/.*found (\d+) rules*/) { print "$1\n" }'`
-    nschemas=`grep "schemas to inspect" $POST_OUT | tail -n 1 \
-        | perl -n -e 'if (/.*?(\d+) schemas to inspect*/) { print "$1\n" }'`
-    schema_stat=`grep "npaths =" $POST_OUT`
-    npaths=`echo $schema_stat | perl -n -e 'if (/npaths = (\d+), min length = (\d+), max length = (\d+), avg length = (\d+)/) { print "$1\n" }'`
-    minlen=`echo $schema_stat | perl -n -e 'if (/npaths = (\d+), min length = (\d+), max length = (\d+), avg length = (\d+)/) { print "$2\n" }'`
-    maxlen=`echo $schema_stat | perl -n -e 'if (/npaths = (\d+), min length = (\d+), max length = (\d+), avg length = (\d+)/) { print "$3\n" }'`
-    avglen=`echo $schema_stat | perl -n -e 'if (/npaths = (\d+), min length = (\d+), max length = (\d+), avg length = (\d+)/) { print "$4\n" }'`
+    stat=`perl <$POST_OUT -e '
+while (<>) {
+    if (/(.*)user (.*)system (.*)elapsed.*avgdata\D*(\d+)maxresident.*/) {
+        $b_user = $1; $b_sys = $2; $b_elapsed = $3; $b_maxres = $4;
+    }
+    if (/PIA domain size: (\d+)/) { $dom_size = $1; }
+    if (/.*the bound for.*is (\d+) =.*/) { $bound = $1; }
+    if (/.*the mild bound for.*is (\d+).*/) { $mbound = $1; }
+    if (/.*the counter abstraction bound for.*is (\d+).*/) { $cabound = $1; }
+    if (/.*the mild counter abstraction bound for.*is (\d+).*/) { $mcabound = $1; }
+    if (/.*the mild counter abstraction bound for.*is (\d+).*/) { $mcabound = $1; }
+    if (/.*?(\d+) unlocking milestones/) { $unlocking = $1; }
+    if (/.*?(\d+) locking milestones/) { $locking = $1; }
+    if (/.*found (\d+) locations.*/) { $nlocs = $1; }
+    if (/.*found (\d+) rules*/) { $nrules = $1; }
+    if (/.*?(\d+) schemas to inspect*/) { $nschemas = $1; }
+    if (/npaths = (\d+), min length = (\d+), max length = (\d+), avg length = (\d+)/) {
+        $npaths = $1; $minlen = $2; $maxlen = $3; $avglen = $4;
+    }
+    if (/found (\d+) non-trivial SCCs/) { $nsccs = $1; }
+}
 
-    echo "10:Result=$res|11:technique=post|40:post-usersec=$b_user|41:post-syssec=$b_sys|42:post-elapsedsec=$b_elapsed|43:post-maxreskb=$b_maxres|45:post-bound=$bound|46:post-unlocking=$unlocking|47:post-locking=$locking|48:post-nlocs=$nlocs|49:post-nrules=$nrules|50:post-cabound=$cabound|51:post-mild-cabound=$mcabound|52:post-mild-bound=$mbound|52:post-nschemas=$nschemas|53:post-minlen=$minlen|54:post-maxlen=$maxlen|55:post-avglen=$avglen"
+print "40:post-usersec=${b_user}|41:post-syssec=${b_sys}\
+|42:post-elapsedsec=${b_elapsed}|43:post-maxreskb=${b_maxres}\
+|45:post-bound=${bound}|46:post-unlocking=${unlocking}\
+|47:post-locking=${locking}|48:post-nlocs=${nlocs}\
+|49:post-nrules=${nrules}\
+|50:post-cabound=${cabound}|51:post-mild-cabound=${mcabound}\
+|52:post-mild-bound=${mbound}|52:post-nschemas=${nschemas}\
+|53:post-minlen=${minlen}|54:post-maxlen=${maxlen}\
+|55:post-avglen=${avglen}|56:post-nsccs=${nsccs}\n"
+'`
+
+    echo "10:Result=$res|$stat"
 }
 
