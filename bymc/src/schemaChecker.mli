655c6786d2fb81d32ae6352ebfdf051c761632c3
konnov <konnov@forsyte.at>
2016-01-14 19:14:01 +0100

    integrated the first version into verifypa-post

--- bymc/src/schemaChecker.mli
+++ bymc/src/schemaChecker.mli
@@ -10,6 +10,12 @@
  @author Igor Konnov, 2014-2016
  *)
 
+open SchemaSmt
+
+
+(**
+ Find a reachable state that violates a specification
+ *)
 val is_error_tree:
     Runtime.runtime_t
         -> SpinIr.data_type_tab
@@ -20,3 +26,44 @@ val is_error_tree:
         -> PorBounds.D.deps_t
         -> PorBounds.T.schema_tree_t
         -> bool
+
+
+(* Retrieve a counterexample from the current SMT context *)
+val get_counterex:
+    Smt.smt_solver -> SymbSkel.Sk.skel_t -> string -> F.frame_t list
+    -> string
+
+(**
+ A simple implementation of tac_t with SMT.
+ *)
+class tree_tac_t: Runtime.runtime_t -> SpinIr.data_type_tab ->
+    object
+        inherit tac_t
+        
+        method top: F.frame_t
+
+        method frame_hist: F.frame_t list
+ 
+        method push_frame: F.frame_t -> unit
+
+        method assert_top:
+            Spin.token SpinIr.expr list -> unit
+
+        method assert_top2:
+            Spin.token SpinIr.expr list -> unit
+
+        method enter_node: node_kind_t -> unit
+
+        method check_property:
+            Spin.token SpinIr.expr -> (F.frame_t list -> unit) -> bool
+
+        method leave_node: node_kind_t -> unit
+
+        method enter_context: unit
+
+        method leave_context: unit
+
+        method push_rule: PorBounds.D.deps_t -> SymbSkel.Sk.skel_t -> int -> unit
+
+    end
+
--- bymc/src/schemaCheckerLtl.mli
+++ bymc/src/schemaCheckerLtl.mli
@@ -1,6 +1,32 @@
+(**
+ A new implementation of a schema-based model checker that supports LTL(F, G)
+ over counters and shared variables.
 
-val check_schema_tree_on_the_fly:
-    SymbSkel.Sk.skel_t -> Spin.token SpinIr.expr
+ Igor Konnov, 2016
+ *)
+
+(**
+ The record type of a result returned by check_schema_tree_on_the_fly.
+ *)
+type result_t = {
+    m_is_err_found: bool;
+    m_counterexample_filename: string;
+}
+
+val find_error:
+    Runtime.runtime_t
+        -> SpinIr.data_type_tab
+        -> SymbSkel.Sk.skel_t -> string
+        -> Spin.token SpinIr.expr
+        -> PorBounds.D.deps_t
+        -> result_t
+   
+(**
+ Enumerate all schemas and try to find a bug.
+ *)
+val gen_and_check_schemas_on_the_fly:
+    Smt.smt_solver
+        -> SymbSkel.Sk.skel_t -> Spin.token SpinIr.expr
         -> PorBounds.D.deps_t -> SchemaSmt.tac_t
-        -> bool
+        -> result_t
 
