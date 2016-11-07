e69332bc9496926ba8555acddd379ac3356f2592
Igor Konnov <konnov@forsyte.at>
2016-11-07 12:10:23 +0100

    the first version of the cegys loop

--- bymc/test/taSyntTest.ml
+++ bymc/test/taSyntTest.ml
@@ -6,6 +6,7 @@ open Spin
 open SpinIr
 open SpinIrImp
 open SymbSkel
+open SchemaSmt
 
 
 let prepare () =
@@ -14,22 +15,23 @@ let prepare () =
     let tt = new data_type_tab in
     let pc = new_var "pc" in
     tt#set_type pc (mk_int_range 0 3);
-    let x, y, n, t = new_var "x", new_var "y", new_var "n", new_var "t" in
+    let x, n, t = new_var "x", new_var "n", new_var "t" in
     let a, b = new_var "a", new_var "b" in
     n#set_symbolic; t#set_symbolic;
     List.iter
         (fun v -> tt#set_type v (new data_type SpinTypes.TUNSIGNED))
-        [x; y; n; t];
+        [x; n; t];
     let add_loc map i =
         let loc = new_var (sprintf "loc%d" i) in
         IntMap.add i loc map
     in
     let locFoo_map = List.fold_left add_loc IntMap.empty (range 0 3) in
-    tt, locFoo_map, pc, x, y, n, t, a, b
+    tt, locFoo_map, pc, x, n, t, a, b
 
 
 let mk_foo params =
-    let tt, locFoo_map, pc, x, y, n, t, a, b = params in
+    (* loc_0 --- x >= an ---> loc_1 --- x >= bt ---> loc_ 2 *)
+    let tt, locFoo_map, pc, x, n, t, a, b = params in
     let mk_eq loc_map loc_no e =
         BinEx (EQ, Var (IntMap.find loc_no loc_map), e)
     in
@@ -37,15 +39,28 @@ let mk_foo params =
         Sk.name = "foo";
         Sk.nlocs = 3; Sk.locs = [ [0]; [1]; [2] ];
         Sk.locals = [ pc ];
-        Sk.shared = [ x; y ];
+        Sk.shared = [ x ];
         Sk.params = [ n; t ];
         Sk.unknowns = [ a; b ];
         Sk.nrules = 0;
-        Sk.rules = [];
+        Sk.rules = [
+            {
+                Sk.src = 0; Sk.dst = 1;
+                Sk.guard = BinEx (GE, Var x, BinEx (MULT, Var a, Var n));
+                Sk.act = [(BinEx (EQ, UnEx (NEXT, Var x),
+                                     BinEx (PLUS, Var x, IntConst 1)))]
+            };
+            {
+                Sk.src = 1; Sk.dst = 2;
+                Sk.guard = BinEx (GE, Var x, BinEx (MULT, Var b, Var t));
+                Sk.act = [(BinEx (EQ, UnEx (NEXT, Var x), Var x))]
+            };
+        ];
         Sk.inits = [
             BinEx (EQ, Var x, IntConst 0);
             mk_eq locFoo_map 0 (BinEx (MINUS, Var n, Var t));
             mk_eq locFoo_map 1 (IntConst 0);
+            mk_eq locFoo_map 2 (IntConst 0);
         ];
         Sk.loc_vars = locFoo_map;
         Sk.assumes = [];
@@ -55,8 +70,8 @@ let mk_foo params =
 
 
 let test_next_unknowns_vec _ =
-    let tt, locFoo_map, pc, x, y, n, t, a, b = prepare () in
-    let sk = mk_foo (tt, locFoo_map, pc, x, y, n, t, a, b) in
+    let tt, locFoo_map, pc, x, n, t, a, b = prepare () in
+    let sk = mk_foo (tt, locFoo_map, pc, x, n, t, a, b) in
     let pairs_s es =
         let p (n, e) = sprintf "(%s, %s)" n (SpinIrImp.expr_s e) in
         "[" ^ (Accums.str_join ", " (List.map p es)) ^ "]"
@@ -113,8 +128,60 @@ let test_next_unknowns_vec _ =
         ~msg:"expected the iterator end"
 
 
+let test_is_cex_applicable _ =
+    let tt, locFoo_map, pc, x, n, t, a, b = prepare () in
+    let sk = mk_foo (tt, locFoo_map, pc, x, n, t, a, b) in
+    let unknowns_vec = [("a", IntConst 2); ("b", IntConst 1)] in
+    let fixed_skel = TaSynt.replace_unknowns sk unknowns_vec in
+    let cex0 = {
+        C.f_loop_index = 1;
+        C.f_init_state =
+            Accums.str_map_of_pair_list StrMap.empty [
+                ("n", 2); ("t", 2); ("x", 2);
+                ("loc_0", 2);  ("loc_1", 0); ("loc_2", 0);
+            ];
+        C.f_moves = [
+            { C.f_rule_no = 0; C.f_accel = 1 };
+            { C.f_rule_no = 1; C.f_accel = 1 };
+        ];
+    }
+    in
+    assert_equal false (TaSynt.is_cex_applicable fixed_skel cex0)
+        ~msg:"expected the counterexample 0 to be inapplicable";
+    let cex1 = {
+        C.f_loop_index = 1;
+        C.f_init_state =
+            Accums.str_map_of_pair_list StrMap.empty [
+                ("n", 2); ("t", 1); ("x", 2);
+                ("loc_0", 2);  ("loc_1", 0); ("loc_2", 0);
+            ];
+        C.f_moves = [
+            { C.f_rule_no = 1; C.f_accel = 1 };
+        ];
+    }
+    in
+    assert_equal true (TaSynt.is_cex_applicable fixed_skel cex1)
+        ~msg:"expected the counterexample 1 to be applicable";
+    let cex2 = {
+        C.f_loop_index = 1;
+        C.f_init_state =
+            Accums.str_map_of_pair_list StrMap.empty [
+                ("n", 1); ("t", 3); ("x", 2);
+                ("loc_0", 2);  ("loc_1", 0); ("loc_2", 0);
+            ];
+        C.f_moves = [
+            { C.f_rule_no = 0; C.f_accel = 1 };
+            { C.f_rule_no = 1; C.f_accel = 1 };
+        ];
+    }
+    in
+    assert_equal true (TaSynt.is_cex_applicable fixed_skel cex2)
+        ~msg:"expected the counterexample 2 to be applicable"
+
+
 let suite = "taSynt-suite" >:::
     [
         "test_next_unknowns_vec" >:: test_next_unknowns_vec;
+        "test_is_cex_applicable" >:: test_is_cex_applicable;
     ]
 
